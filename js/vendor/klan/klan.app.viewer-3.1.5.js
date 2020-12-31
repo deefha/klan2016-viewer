4510c36ea1bc9144add3c6d20e05b2808acb5b95
Daniel Felix Hrouzek <de.ef.ha@gmail.com>
2020-12-31 12:54:53 +0100

    Add image title as zoom caption

--- js/vendor/klan/klan.app.viewer-3.1.5.js
+++ js/vendor/klan/klan.app.viewer-3.1.5.js
@@ -650,7 +650,7 @@ $.klan.app.viewer = function(element, options) {
 					);
 
 					output_library.push(sprintf(
-						'<div id="image-%s" class="item item-image"><div class="meta">#%s %sx%s M%s<br />%s</div><div class="data"><a href="#/%s/%s/%s/%s"><img src="%s" data-original="%s" style="padding-top:%spx;" /></a></div></div>',
+						'<div id="image-%s" class="item item-image"><div class="meta">#%s %sx%s M%s<br />%s</div><div class="data"><a href="#/%s/%s/%s/%s"><img src="%s" alt="%s" data-original="%s" style="padding-top:%spx;" /></a></div></div>',
 						image_index,
 						image_index,
 						image.width,
@@ -662,6 +662,7 @@ $.klan.app.viewer = function(element, options) {
 						plugin.actual.index,
 						image_index,
 						image_url_thumbnail,
+						image.title ? image.title : '- no title -',
 						image_url_original,
 						Math.round((image_max_height - image_display_height) / 2)
 					));
@@ -881,6 +882,9 @@ $.klan.app.viewer = function(element, options) {
 				),
 				{
 					type: 'image',
+					afterContent: function(event) {
+						$('<div class="title">').text(image.attr('alt')).appendTo(this.$instance.find('.featherlight-content'));
+					},
 					afterClose: function(event) {
 						hasher.replaceHash(sprintf('%s/%s/%s', plugin.actual.issue, plugin.actual.library, plugin.actual.index));
 					}
